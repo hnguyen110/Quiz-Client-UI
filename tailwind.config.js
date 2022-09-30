47c973c6672ce5eaf6dcbc6d8e93ac7a13c34fb4
Hien Nguyen <hnguyen110.akc@gmail.com>
2022-09-30 15:05:32 -0400

    Set up client UI project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{html,js,jsx,ts,tsx}",
+    "./pages/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
