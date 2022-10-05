b359a1d96bbf29b90723d445fdf5a2c4e83b7077
Hien Nguyen <hnguyen110.akc@gmail.com>
2022-10-04 22:22:33 -0400

    Fixed the answers context to get the user answers from a multiple choice question and single choice question

--- components/quizzes/quiz-multiple-choice-question/quiz-multiple-choice-question.tsx
+++ components/quizzes/quiz-multiple-choice-question/quiz-multiple-choice-question.tsx
@@ -1,4 +1,7 @@
-import { Checkbox, Space } from "antd";
+import { Checkbox, RadioChangeEvent, Space } from "antd";
+import { CheckboxValueType } from "antd/lib/checkbox/Group";
+import { useContext } from "react";
+import { QuizAnswersContext } from "../../../contexts/quiz-answers.context";
 import Question from "../../../utilities/types/quizzes/question.type";
 
 interface Props {
@@ -6,8 +9,30 @@ interface Props {
 }
 
 export default function QuizMultipleChoiceQuestion({ question }: Props) {
+  const { answers, setAnswers } = useContext(QuizAnswersContext);
+
+  function onChangeHandler(values: any) {
+    const answer = answers.find((item) => item.question === question.id);
+    if (answer) {
+      const temp = answers.filter((item) => item.question !== question.id);
+      setAnswers([
+        ...values.map((item: number) => {
+          return { question: question.id, selected_solution: item };
+        }),
+        ...temp,
+      ]);
+    } else {
+      setAnswers([
+        ...values.map((item: number) => {
+          return { question: question.id, selected_solution: item };
+        }),
+        ...answers,
+      ]);
+    }
+  }
+
   return (
-    <Checkbox.Group>
+    <Checkbox.Group onChange={onChangeHandler}>
       <Space direction="vertical">
         {question.solutions.map((item) => (
           <Checkbox key={item.id} value={item.id}>
