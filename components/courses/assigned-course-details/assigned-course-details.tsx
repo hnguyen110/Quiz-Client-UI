a9daf4df45f85e081168684a92658ffdbc7b2186
Hien Nguyen <hnguyen110.akc@gmail.com>
2022-10-18 16:02:44 -0400

    Integrated the API to retrieve the pre-signed url for a media content when the media item is requested to the front-end

--- components/courses/assigned-course-details/assigned-course-details.tsx
+++ components/courses/assigned-course-details/assigned-course-details.tsx
@@ -6,6 +6,17 @@ import { getAssignedCourseDetails } from "../../../services/courses/assigned-cou
 import { useSession } from "next-auth/react";
 import AssignedCourseSection from "../../../utilities/types/courses/assigned-course-section.type";
 import AssignedCourseSectionItem from "../../../utilities/types/courses/assigned-course-section-item.type";
+import { getCourseSectionItem } from "../../../services/courses/course-section-items.service";
+import CourseSectionItem from "../../../utilities/types/courses/course-section-item.type";
+import GenericSpin from "../../utilities/generic-spin/generic-spin";
+import dynamic from "next/dynamic";
+
+const VideoPlayer = dynamic(
+  () => import("../../utilities/video-player/video-player"),
+  {
+    ssr: false,
+  }
+);
 
 const { TreeNode } = TreeSelect;
 
@@ -21,28 +32,47 @@ export default function AssignedCourseDetails({
   setOpen,
 }: Props) {
   const [sections, setSections] = useState([] as AssignedCourseSection[]);
+  const [section, setSection] = useState(null as null | AssignedCourseSection);
   const [item, setItem] = useState(null as null | AssignedCourseSectionItem);
+  const [mediaContent, setMediaContent] = useState(
+    null as null | CourseSectionItem
+  );
+  const [paused, setPaused] = useState(true);
   const session = useSession();
 
   useEffect(() => {
-    getAssignedCourseDetails(session.data as any, course?.id)
-      .then((data) => {
-        setSections(data.sections);
-        if (item === null) {
-          setItem(data.sections[0].items[0]);
-        }
-      })
-      .catch((e) => {
-        message.error(
-          "There was an issue while trying to receive the course details, please try again"
-        );
-      });
-  }, [course?.id, item, session.data]);
+    if (course) {
+      getAssignedCourseDetails(session.data as any, course?.id)
+        .then((data) => {
+          setSections(data.sections);
+          if (item === null) {
+            setSection(data.sections[0]);
+            setItem(data.sections[0].items[0]);
+          }
+        })
+        .catch((e) => {
+          message.error(
+            "There was an issue while trying to receive the course details, please try again"
+          );
+        });
+    }
+  }, [course, item]);
+
+  useEffect(() => {
+    if (section && item) {
+      getCourseSectionItem(session.data as any, course.id, section.id, item.id)
+        .then((data) => {
+          setMediaContent(data);
+        })
+        .catch();
+    }
+  }, [course?.id, item, section]);
 
   async function onCourseSectionItemSelectedHandler(id: string | number) {
-    sections.map((section) => {
-      section.items.map((sectionItem) => {
+    sections.map((courseSection) => {
+      courseSection.items.map((sectionItem) => {
         if (sectionItem.id === id) {
+          setSection(courseSection);
           setItem(sectionItem);
         }
       });
@@ -53,9 +83,14 @@ export default function AssignedCourseDetails({
     <GenericDrawer
       title={course?.title || ""}
       placement="right"
-      width="100%"
+      width="95%"
       open={open}
-      onCloseHandler={() => setOpen(false)}
+      onCloseHandler={() => {
+        setPaused(true);
+        setTimeout(() => {
+          setOpen(false);
+        }, 1);
+      }}
       extra={
         <TreeSelect
           showArrow={false}
@@ -81,13 +116,17 @@ export default function AssignedCourseDetails({
         </TreeSelect>
       }
     >
-      {/*<VideoPlayer*/}
-      {/*  id={item.id}*/}
-      {/*  source={item.}*/}
-      {/*  type={"video"}*/}
-      {/*  paused={true}*/}
-      {/*  setPaused={null}*/}
-      {/*/>*/}
+      {mediaContent ? (
+        <VideoPlayer
+          id={mediaContent.id}
+          source={mediaContent.data}
+          type={mediaContent.content_type}
+          paused={paused}
+          setPaused={setPaused}
+        />
+      ) : (
+        <GenericSpin />
+      )}
     </GenericDrawer>
   );
 }
